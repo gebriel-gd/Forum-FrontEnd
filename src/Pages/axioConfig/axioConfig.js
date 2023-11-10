431c8c39e21fa98a2c12a3c2046d4b2ae11163e5
gebriel-gd <gebrield@gmail.com>
2023-11-10 05:31:51 -0500

    port and env update

--- src/Pages/axioConfig/axioConfig.js
+++ src/Pages/axioConfig/axioConfig.js
@@ -1,8 +1,6 @@
-
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api/users", // Set your base URL
-  
+  baseURL: "https://aware-jade-starfish.cyclic.app/api/users", // Set your base URL
 });
 
 export default axiosInstance;
